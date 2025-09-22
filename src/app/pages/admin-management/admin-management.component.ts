a747d961053dc91f97dc279f692925cbf5105424
leezh <22004832@siswa.um.edu.my>
2025-09-22 20:27:25 +0800

    Create admin form UI

--- src/app/pages/admin-management/admin-management.component.ts
+++ src/app/pages/admin-management/admin-management.component.ts
@@ -1,21 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardModule } from "primeng/card";
 import { UserService } from '../../services/user.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
+import { Dialog } from 'primeng/dialog';
+import { InputText } from "primeng/inputtext";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToggleSwitchModule } from 'primeng/toggleswitch'
 
 @Component({
   selector: 'app-admin-management',
-  imports: [CardModule, NgIf, AsyncPipe, ButtonModule],
+  imports: [CardModule, NgIf, AsyncPipe, ButtonModule, Dialog, InputText, ReactiveFormsModule, CommonModule, ToggleSwitchModule],
   templateUrl: './admin-management.component.html',
   styleUrl: './admin-management.component.scss'
 })
-export class AdminManagementComponent {
-  constructor(public userService: UserService, private router: Router) {}
+export class AdminManagementComponent implements OnInit {
+  visible: boolean = false;
+  createAdminForm!: FormGroup;
+
+  constructor(public userService: UserService, private router: Router, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createAdminForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      isSuperadmin: [false],
+    })
+  }
 
   async onLogout() {
     await this.userService.logout();
     this.router.navigate(['/login']);
+  }
+  
+  async onCreateAdmin() {
+    this.onCloseCreateAdminDialog();
+  }
+
+  showCreateAdminDialog() {
+    this.visible = true;
+  }
+
+  onCloseCreateAdminDialog() {
+    this.visible = false;
+    this.createAdminForm.reset({
+      username: '',
+      email: '',
+      password: '',
+      isSuperadmin: false,
+    });
+  }
+
+  isFieldInvalid(controlName: string): boolean {
+    const control = this.createAdminForm.get(controlName);
+    return !!(control && control.touched && control.invalid)
   }
 }
