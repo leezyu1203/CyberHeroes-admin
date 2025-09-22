333a8071bc1ae5c4ccd830fb95f0250e84f255fd
leezh <22004832@siswa.um.edu.my>
2025-09-22 09:36:29 +0800

    Admin Management layout

--- src/app/pages/admin-management/admin-management.component.ts
+++ src/app/pages/admin-management/admin-management.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CardModule } from "primeng/card";
+import { UserService } from '../../services/user.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-admin-management',
-  imports: [],
+  imports: [CardModule, NgIf, AsyncPipe, ButtonModule],
   templateUrl: './admin-management.component.html',
   styleUrl: './admin-management.component.scss'
 })
 export class AdminManagementComponent {
-
+  constructor(public userService: UserService) {}
 }
