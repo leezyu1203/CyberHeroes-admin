36cd0ec3b1c0a82952282a06ca60dca3311235d7
leezh <22004832@siswa.um.edu.my>
2025-09-23 13:57:28 +0800

    Retrieve FilterForce messages

--- src/app/pages/filter-force/filter-force.component.ts
+++ src/app/pages/filter-force/filter-force.component.ts
@@ -5,24 +5,44 @@ import { ButtonModule } from 'primeng/button';
 import { Dialog } from 'primeng/dialog'
 import { InputTextModule } from 'primeng/inputtext';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { FilterForceService, Message } from '../../services/filter-force.service';
+import { TableModule } from 'primeng/table';
+import { BadgeModule } from 'primeng/badge';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { SkeletonModule } from 'primeng/skeleton'
 
 @Component({
   selector: 'app-filter-force',
-  imports: [ButtonModule, Dialog, ToggleSwitchModule, ReactiveFormsModule, InputTextModule, CommonModule],
+  imports: [ButtonModule, Dialog, ToggleSwitchModule, ReactiveFormsModule, InputTextModule, CommonModule, TableModule, BadgeModule, ToastModule, SkeletonModule],
   templateUrl: './filter-force.component.html',
-  styleUrl: './filter-force.component.scss'
+  styleUrl: './filter-force.component.scss',
+  providers: [MessageService]
 })
 export class FilterForceComponent implements OnInit {
   visible: boolean = false;
+  isLoading: boolean = true;
   createMessageForm!: FormGroup;
+  messages: Message[] = [] ;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private ffService: FilterForceService, private messageService: MessageService) {}
 
   ngOnInit() {
     this.createMessageForm = this.fb.group({
       message: ['', [Validators.required]],
       isDanger: [false],
     });
+    this.ffService.getMessages().subscribe({
+      next: msgs => {
+        this.messages = msgs;
+        console.log(this.messages);
+        this.isLoading = false;
+      },
+      error: err => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: err.message, life: 3000 });
+        this.isLoading = false;
+      }
+    })
   }
 
   async onCreateMessage() {
--- src/app/services/filter-force.service.ts
+++ src/app/services/filter-force.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Message {
+  id?: string;
+  message: string;
+  is_danger: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterForceService {
+  private firestore = inject(Firestore);
 
   constructor() { }
+
+  getMessages(): Observable<Message[]> {
+    const messagesRef = collection(this.firestore, 'MG001_messages');
+    return collectionData(messagesRef, { idField: 'id' }) as Observable<Message[]>;
+  }
 }
